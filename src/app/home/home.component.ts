a5848f69eb865a1f861197f6505bd4dbeccb0918
Thomk <thomkok13@hotmail.com>
2020-09-02 17:40:12 +0200

    temporary json object with attractions added.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  items = ['', '', '', '', ''];
+  attractions = [
+    {
+      name: 'Reuzenrad',
+      description: 'Dit is een reuzenrad!',
+      madeBy: 'Thom Kok',
+      image: 'Reuzenrad.png',
+      alt: 'reuzenrad'
+    },
+    {
+      name: 'Zweefmolen',
+      description: 'Dit is een zweefmolen!',
+      madeBy: 'Thom Kok',
+      image: 'Zweefmolen.png',
+      alt: 'zweefmolen'
+    },
+    {
+      name: 'Achtbaan',
+      description: 'Dit is een achtbaan!',
+      madeBy: 'Thom Kok',
+      image: 'Achtbaan.png',
+      alt: 'achtbaan'
+    },
+    {
+      name: 'Ghostship',
+      description: 'Dit is een schip!',
+      madeBy: 'Thom Kok',
+      image: 'Ghostship.png',
+      alt: 'ghostship'
+    },
+    {
+      name: 'Locomotief',
+      description: 'Dit is een locomotief!',
+      madeBy: 'Thom Kok',
+      image: 'Locomotief.png',
+      alt: 'locomotief'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
